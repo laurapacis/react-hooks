260f56acf7d022434c8c5e8cbf186c5105889e0d
laurapacis <laural@tuta.io>
2022-02-13 20:27:33 +0100

    7. useEffect: HTTP request – 3.Extra Credit: Store the State in an Object

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -5,24 +5,25 @@ import * as React from 'react'
 import {PokemonForm, fetchPokemon, PokemonInfoFallback, PokemonDataView,} from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
-    const [status, setStatus] = React.useState('idle')
-    const [pokemon, setPokemon] = React.useState(null) // before calling `fetchPokemon`, clear the current pokemon state by setting it to null. (This is to enable the loading state when switching between different pokemon.)
-    const [error, setError] = React.useState(null)
+    const [state, setState] = React.useState({
+        status: 'idle',
+        pokemon: null,
+        error: null
+    })
+    const {status, pokemon, error} = state
 
     React.useEffect(() => { // use React.useEffect where the callback should be called whenever the pokemon name changes.
         if (!pokemonName) {
             return // if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
         }
-        setStatus('pending')
+        setState({status: 'pending'})
      
         fetchPokemon(pokemonName)
         .then(pokemon => {
-            setPokemon(pokemon)
-            setStatus('resolved')
+            setState({pokemon, status: 'resolved'})
         })
         .catch(error => {
-            setError(error)
-            setStatus('rejected')
+            setState({error, status: 'rejected'})
         })
     },[pokemonName])
     
