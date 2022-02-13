47997d8653c4ec2a51dfb3f76181fbb6e0be2a22
laurapacis <laural@tuta.io>
2022-02-13 18:47:20 +0100

    7. useEffect: HTTP request – 1.Extra Credit: Handle Errors

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -6,16 +6,25 @@ import {PokemonForm, fetchPokemon, PokemonInfoFallback, PokemonDataView,} from '
 
 function PokemonInfo({pokemonName}) {
   const [pokemon, setPokemon] = React.useState(null) // before calling `fetchPokemon`, clear the current pokemon state by setting it to null. (This is to enable the loading state when switching between different pokemon.)
+  const [error, setError] = React.useState(null)
 
   React.useEffect(() => { // use React.useEffect where the callback should be called whenever the pokemon name changes.
     if (!pokemonName) {
         return // if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
     }
     setPokemon(null) // to show that loading state
-    fetchPokemon(pokemonName).then(pokemon => setPokemon(pokemon))
+    fetchPokemon(pokemonName)
+    .then(pokemon => setPokemon(pokemon))
+    .catch(error => setError(error))
   },[pokemonName])
  
- 
+if (error) {
+    return (
+    <div role="alert">
+     There was an error: <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+    </div>
+    )
+}
 if (!pokemonName) {
     return 'Submit a pokemon'
 } else if (!pokemon) {
